27effe520a3147d56dcd457633156c7961a908af
michiya-59 <nishino.michiya0509@gmail.com>
2021-09-22 10:38:29 +0900

    topページの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post,only: [:edit,:update,:show,:destroy]
   def index
-    @posts = Post.all
+    @posts = current_user.posts.all
+    
   end
 
   def new
