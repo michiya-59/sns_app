f5e22ec37a72ab6e603effb4c9a6db941beebaaf
michiya-59 <nishino.michiya0509@gmail.com>
2021-09-22 23:43:36 +0900

    フォローワー、フォローのコード実装途中

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
   has_many :posts,dependent: :destroy
+  has_many :active_relationships,class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships,class_name: "Relationship",foreign_key: "followed_id",dependent: :destroy
+  has_many :following, through: :active_relationships,source: :followed
+  has_many :followers,through: :passive_relationships,source: :follower
+
   validates :name,presence: true,length: {maximum: 20}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,presence: true,length: {maximum: 255},format: {with: VALID_EMAIL_REGEX},uniqueness: { case_sensitive: false }
@@ -31,5 +36,17 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest,nil)
+  end
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_120655) do
+ActiveRecord::Schema.define(version: 2021_09_22_050832) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
