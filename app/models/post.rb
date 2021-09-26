a8c6ff46c2b74b2ece3df205d436bf6046ac9a85
michiya-59 <nishino.michiya0509@gmail.com>
2021-09-26 19:39:45 +0900

    それぞれのモデルについてに記述と、ルーティングの記入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
 
   def user
     return User.find_by(id: self.user_id)
+  end
+
+  def liked_by?(current_user)
+    likes.where(user_id: current_user.id).exists?
   end
 end
