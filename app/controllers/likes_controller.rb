91d0f9bf5d3302b1bcbf82306c37d863a126e351
michiya-59 <nishino.michiya0509@gmail.com>
2021-09-26 20:48:40 +0900

    いいね機能の完成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,19 @@
 class LikesController < ApplicationController
 
   def create
-    
+    @post = Post.find(params[:post_id])
+    unless @post.liked_by?(current_user)
+      like = current_user.likes.new(post_id: @post.id)
+      like.save
+      redirect_to @post
+    end
   end
 
   def destroy
-    
+    @post = Post.find(params[:post_id])
+    like = current_user.likes.find_by(post_id: @post.id)
+    like.destroy
+    redirect_to @post
   end
+  
 end
