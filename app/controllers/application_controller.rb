3b3f083eb8965db92be299fe41e1548545b9f9b2
Daisuke Tsuji <dim0627@gmail.com>
2021-09-29 13:44:52 +0900

    typoしてた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   include(SessionsHelper)
 
   def redirect_when_no_logged_in
-    returun if current_user
+    return if current_user
 
     redirect_to login_path
   end
