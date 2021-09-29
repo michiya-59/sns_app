d4c5ef929d049c44e6a62762e3cd487967175dc0
Daisuke Tsuji <dim0627@gmail.com>
2021-09-29 19:11:16 +0900

    DOMとJSの対応

--- app/javascript/src/like.js
+++ app/javascript/src/like.js
@@ -1,19 +1,35 @@
+// 条件に応じて2つのボタン（Like, CancelLike）の表示を切り替える
+const switching = (isLiked, likeButton, cancelLikeButton) => {
+  if (isLiked) {
+    likeButton.classList.add('h-hide')
+    cancelLikeButton.classList.remove('h-hide')
+  } else {
+    likeButton.classList.remove('h-hide')
+    cancelLikeButton.classList.add('h-hide')
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const likeContainers = document.querySelectorAll('[data-like-container]')
+
   Array.from(likeContainers).forEach(likeContainer => {
     const likeButton = likeContainer.querySelector('[data-like]')
-    const cancelLikeButton = likeContainer.querySelector('[data-like]')
+    const cancelLikeButton = likeContainer.querySelector('[data-cancel-like]')
+    // それぞれのボタンが２個あるので、Countを表示しているDOMも2個ある
+    const countSpans = likeContainer.querySelectorAll('[data-count]')
+    // HACK: 文字列でBooleanが入っているのでevalしてBooleanに変換する
+    const isLiked = eval(likeContainer.dataset.isLiked)
 
     likeButton.addEventListener('ajax:success', () => {
-      console.log('liked!')
-      // TODO: Likeボタンを非表示にしてLikeCancelボタンを表示する
+      Array.from(countSpans).forEach(countSpan => countSpan.innerHTML = eval(countSpan.innerHTML + 1))
+      switching(true, likeButton, cancelLikeButton)
     })
-
     cancelLikeButton.addEventListener('ajax:success', () => {
-      console.log('like canceled!')
-      // TODO: LikeCancelボタンを非表示にしてLikeボタンを表示する
+      Array.from(countSpans).forEach(countSpan => countSpan.innerHTML = eval(countSpan.innerHTML - 1))
+      switching(false, likeButton, cancelLikeButton)
     })
 
-    // TODO: Like済みかどうかを判定して、どちらを表示しておくかを処理する
+    // Like済みかどうかを判定して、どちらを表示しておくかを処理する
+    switching(isLiked, likeButton, cancelLikeButton)
   })
 })
