529d952eb3dd1aa0983e1ed42cc3f7752926179d
michiya-59 <nishino.michiya0509@gmail.com>
2021-09-29 10:29:06 +0900

    ログインしていない場合の処置

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   include(SessionsHelper)
+
+  def logged_user_in
+    if current_user.nil?
+      redirect_to login_path
+    end
+  end
+
 end
