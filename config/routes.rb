9f32370bacf727e0ef642e198bd5670a614d41be
michiya-59 <nishino.michiya0509@gmail.com>
2021-10-02 18:18:36 +0900

    フォロー機能を実装するためにrelationshipsのコントローラー作成とルートの作成

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
+
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
