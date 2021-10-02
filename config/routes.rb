f6bfa37842492c575e45ae7f80c85aca85198910
michiya-59 <nishino.michiya0509@gmail.com>
2021-10-02 18:26:13 +0900

    relationshipsのルートを変更し、Viewのlink_toを記載

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,9 @@ Rails.application.routes.draw do
       get 'setting'
       get :following, :followers
     end
+    resources :relationships, only: [:create, :destroy]
   end
 
-  resources :relationships, only: [:create, :destroy]
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
