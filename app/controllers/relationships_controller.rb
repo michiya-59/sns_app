f2756327b37ca477b6668b904379ac1c941d1e8b
michiya-59 <nishino.michiya0509@gmail.com>
2021-10-03 14:51:12 +0900

    例外処理の入れた

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,18 @@
 class RelationshipsController < ApplicationController
   def create
-    current_user.follow(params[:user_id])
-    redirect_to request.referer
+    if current_user.following?(params[:user_id])
+      redirect_to user_path(params[:user_id]) #フォローされていたらユーザのプロフィールにリダイレクト
+    else
+      current_user.follow(params[:user_id])
+      redirect_to user_path(params[:user_id])
+    end
   end
 
   def destroy
-    current_user.unfollow(params[:user_id]).destroy
-    redirect_to request.referer
+    if current_user.following?(params[:user_id])
+      current_user.unfollow(params[:user_id]).destroy #フォローされていたら、フォローを外すようにしている
+      redirect_to user_path(params[:user_id])
+    else
+      redirect_to user_path(params[:user_id])
   end
 end
