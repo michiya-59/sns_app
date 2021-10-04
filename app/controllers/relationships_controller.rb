fd27c4951e924e5c40bb74f8f98102f325283ec8
michiya-59 <nishino.michiya0509@gmail.com>
2021-10-04 20:42:49 +0900

    例外処理の記述

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,20 @@
 class RelationshipsController < ApplicationController
   def create
-    current_user.follow(params[:user_id])
-    head :ok
+    if current_user.following?(params[:user_id])
+      head :ok
+    else
+      current_user.follow(params[:user_id])
+      head :ok
+    end
+    
   end
 
   def destroy
-    current_user.unfollow(params[:user_id]).destroy
-    head :ok
+    if current_user.following?(params[:user_id])
+      current_user.unfollow(params[:user_id]).destroy
+      head :ok
+    else
+      head :ok
+    end
   end
 end
