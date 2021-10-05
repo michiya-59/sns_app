7cefa8d3b816887b7c54a1a7e64d7392ba44c06b
michiya-59 <nishino.michiya0509@gmail.com>
2021-10-05 14:47:25 +0900

    following?の変更”

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,7 +49,7 @@ class User < ApplicationRecord
     active_relationships.find_by(followed_id: user_id)
   end
 
-  def following?(user)
-    following.include?(user)
+  def following?(other_id)
+    active_relationships.where(followed_id: other_id).exists?
   end
 end
